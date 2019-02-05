7e1679c52592d20c4ed8402ec6c97bf95c45315c
Ron Lynch <lynch.ronald1@gmail.com>
2019-02-05 15:58:09 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,57 @@
 class Owner
   # code goes here
+  attr_accessor :name
+  attr_reader :species
+  @@all = []
+  def species
+
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+
+  end
+
+  def name(name)
+    
+  end
+
+  def pets
+
+  end
+
+  def buy_fish
+
+  end
+
+  def buy_cat
+
+  end
+
+  def buy_dog
+
+  end
+
+  def walk_dogs
+
+  end
+
+  def play_with_cats
+
+  end
+
+  def feed_fish
+
+  end
+
+  def sell_pets
+
+  end
+
+  def list_pets
+
+  end
 end
